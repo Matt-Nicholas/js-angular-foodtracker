2c529e4e2d01b347acf7d2b69685482ac270b53b
Matt Nicholas <fostermatt82@gmial.com>
2016-10-20 18:00:42 -0700

    init local repo

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Eat It!</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
